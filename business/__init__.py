e25fbe4cd046290208075abf5c348f56d6056d8c
Felgine Touko Lina <touk0001@algonquinlive.com>
2026-02-22 16:55:46 -0500

    Add business layer with DataManager CRUD operations- Created DataManager class managing in-memory record collection- Implemented Create: create_record() adds new OystercatcherRecord- Implemented Read: get_all_records(), get_record_by_index()- Implemented Update: update_record() modifies existing record- Implemented Delete: delete_record() removes record by index- Implemented load_data() and save_data() coordinating with persistence layer- All methods return (success: bool, message: str) tuplesAuthor: Felgine Touko LinaCourse: CST8002

---
+++ business/__init__.py
@@ -0,0 +1,12 @@
+"""
+Author: Felgine Touko Lina
+Course: CST8002 - Programming Language Research
+Professor: Stanley Pieda
+Due Date: 02-22-2026
+Description: package initialization file for business layer.
+
+References:
+[1] Parks Canada, “Black Oystercatcher Population – Pacific Rim,”open.canada.ca,
+Oct. 01, 2017. [Online].Available: https://open.canada.ca/data/en/dataset/d87383f6-
+5313-430d-8416-1b6d6e377e02. [Accessed: Dec. 23, 2025].
+"""
